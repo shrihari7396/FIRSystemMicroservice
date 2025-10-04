0b3fcdfbdbf9fcb23edc30a08c8703764de4ebbd
SHRIHARI KULKARNI <shriharik121@gmail.com>
2025-10-04 23:03:49 +0530

    GenAi Service Initiated

---
+++ GenAiService/src/main/java/edu/pict/genaiservice/GenAiServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.pict.genaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiServiceApplication.class, args);
+	}
+
+}
