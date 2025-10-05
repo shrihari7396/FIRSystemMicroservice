42b2b4191e1f0a14525ae81681e8cca3703829c0
SHRIHARI KULKARNI <shriharik121@gmail.com>
2025-10-05 23:49:31 +0530

    Coomplat Service Implementaiton Setarted

---
+++ ComplaintSerivce/src/main/java/edu/pict/complaintserivce/ComplaintSerivceApplication.java
@@ -0,0 +1,13 @@
+package edu.pict.complaintserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintSerivceApplication.class, args);
+	}
+
+}
---
+++ ComplaintSerivce/src/test/java/edu/pict/complaintserivce/ComplaintSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pict.complaintserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
