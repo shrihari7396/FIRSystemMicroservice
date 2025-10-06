4e145afc587c20c7be3eb27439d157e14d3e230f
SHRIHARI KULKARNI <shriharik121@gmail.com>
2025-10-06 17:43:50 +0530

    Email Service

--- EmailService/src/main/java/edu/pict/emailservice/EmailServiceApplication.java
+++ EmailService/src/main/java/edu/pict/emailservice/EmailServiceApplication.java
@@ -2,14 +2,14 @@ package edu.pict.emailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
 @EnableCaching
 public class EmailServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
-
 }
